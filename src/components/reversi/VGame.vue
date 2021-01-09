125e8792ee8bd1a50a6dccd8d2abae460325795e
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-09 21:49:12 +0900

    ターンの表示実装

--- src/components/reversi/VGame.vue
+++ src/components/reversi/VGame.vue
@@ -7,13 +7,18 @@
       :board="board"
       />
     </v-layout>
+    <v-layout
+    justify-center>
+      <h2 class="mt-5">{{current}}</h2>
+    </v-layout>
+
   </v-container>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 import VBoard from '@/components/reversi/VBoard.vue';
-import { Board } from '@/models/reversi';
+import { Board, CellState } from '@/models/reversi';
 
 @Component ({
   components: {
@@ -23,6 +28,14 @@ import { Board } from '@/models/reversi';
 export default class VGame extends Vue {
 
   public board: Board = new Board();
+
+  public get current() {
+    if (this.board.turn === CellState.White) {
+      return '白色の手番'
+    } else {
+      return '黒色の手番'
+    }
+  }
 }
 
 </script>
